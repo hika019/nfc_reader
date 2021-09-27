be7e5012c2b7a558824ec68d79568efeb95b73bb
hika019 <ikazuti2001@gmail.com>
2021-09-28 01:47:50 +0900

    IDmの読み取り(免許は読めない・・・)

--- nfc/ソース.cpp
+++ nfc/ソース.cpp
@@ -10,7 +10,26 @@ using LazyPCSCFelicaLite::PCSCCryptographicException;
 using LazyPCSCFelicaLite::FelicaErrorException;
 using LazyPCSCFelicaLite::FelicaFatalException;
 using LazyPCSCFelicaLite::PCSCCardRemovedException;
+using namespace std;
 
 int main() {
+	PCSCFelicaLite f = PCSCFelicaLite();
+
+	try {
+		cout << "‚©‚´‚µ‚Ä\n";
+		f.autoConnectToFelica(INFINITE);
+
+		uint8_t uid[256] = { 0 };
+		int len = f.readUID(uid);
+
+		for (int i = 0; i < len; i++) {
+			printf("%02X ", uid[i]);
+		}
+		printf("\n");
+
+	}catch (std::runtime_error e) {
+		cout << e.what() << "\n";
+	}
+
 	return 0;
 }
