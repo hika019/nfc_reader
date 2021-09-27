8dc3ff26c156dd426ad17dec651f996e012c396b
hika019 <ikazuti2001@gmail.com>
2021-09-28 01:04:38 +0900

    プロジェクト ファイルを追加します。

---
+++ nfc/ソース.cpp
@@ -0,0 +1,16 @@
+#include "lazypcscfelicalite.h"
+#include <iostream>
+
+using LazyPCSCFelicaLite::PCSCFelicaLite;
+using LazyPCSCFelicaLite::PCSCErrorException;
+using LazyPCSCFelicaLite::PCSCIllegalStateException;
+using LazyPCSCFelicaLite::PCSCCommandException;
+using LazyPCSCFelicaLite::PCSCFatalException;
+using LazyPCSCFelicaLite::PCSCCryptographicException;
+using LazyPCSCFelicaLite::FelicaErrorException;
+using LazyPCSCFelicaLite::FelicaFatalException;
+using LazyPCSCFelicaLite::PCSCCardRemovedException;
+
+int main() {
+	return 0;
+}
